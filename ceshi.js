cec4af4304e29845d7a4221272cbd2941149361d
afeng <zhanghuiyu997@gmail.com>
2024-03-02 23:55:22 +0800

    跟新配置文件

--- ceshi.js
+++ ceshi.js
@@ -1,1 +1,11 @@
+const fs = require('fs');
+const iniParser = require('ini-parser');
+
+
+// 读取 INI 文件
+const configData = fs.readFileSync('./config/config.ini', 'utf-8');
+
+const parsedData = iniParser.parse(configData);
+// 解析 INI 格式的数据
+console.log(parsedData);
 
--- config/index.js
+++ config/index.js
@@ -3,15 +3,15 @@ module.exports = {
     EndSending:"endset", //结束发送命令
     sendtimes: [
         {
-            hour: 21,
-            minute: 16
+            hour: 22,
+            minute: 50
         }, {
             hour: 21,
             minute: 57
         }
     ], //指定发送时间 sendtype为1时生效
     interval: '*/10 * * * * *', //间隔5秒发送一次  可自行调整  sendtype为2时生效 第三方库固定格式 不可更改
-    sendtype: 2, //发送类型  1、指定时间发送  2、间隔n秒发送一次
+    sendtype: 1, //发送类型  1、指定时间发送一次  2、间隔n秒发送一次
     Chatid: "",//发送目标
     content: "", //消息内容
     type: 2, //广告类型 1图片 2文本
@@ -26,4 +26,4 @@ module.exports = {
        
     ],//配置广告按钮
     Permiss:['用户chatid'] //开启群发用户权限
-} 
+}
